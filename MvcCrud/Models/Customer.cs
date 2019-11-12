eccf8c086f96af47963020853df69a41e877bf0e
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-11-12 23:43:15 +0100

    index, layout modifications

--- MvcCrud/Models/Customer.cs
+++ MvcCrud/Models/Customer.cs
@@ -17,5 +17,8 @@ namespace MvcCrud.Models
         public int CustomerID { get; set; }
         public string CustomerName { get; set; }
         public string City { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
